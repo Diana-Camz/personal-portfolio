6b6c28c33f0adc97f17da5b9fea033bb75e45fca
Diana-Camz <dcampos0495@gmail.com>
2025-10-03 21:19:35 -0600

    feat: configure files to allow installation of starwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+//import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
    vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@': '/src',
